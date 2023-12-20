d92b2bdae6b8312e4d6529cdfc5707027eff6d99
BriseydaMF <briseyda.flores29@gmail.com>
2023-12-19 21:39:10 -0600

    test

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,19 @@
-import { Container } from "react-bootstrap";
-import Header from "./components/Header";
+import { Container } from 'react-bootstrap';
+import Header from './components/Header';
 
 import React from 'react'
 
 
 const App = () => {
   return (
-    <>
+  <>
     <Header />
-    <main className="py-3">
+    <main className='py-3'>
       <Container>
   <h1>Welcome To Proshop</h1>
       </Container>
     </main>
-    </>
+  </>
   );
 };
 export default App;
