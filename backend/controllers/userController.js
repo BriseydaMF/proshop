3679cd046071ffee8b4844148a6de94ee0b2dd6d
BriseydaMF <briseyda.flores29@gmail.com>
2024-03-14 20:43:45 -0600

    test

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -40,14 +40,41 @@ const authUser = asyncHandler(async (req, res) => {
 // @route   POST /api/users
 // @access  Public
 const registerUser = asyncHandler(async (req, res) => {
-    res.send('register user');
+    const { name, email, body } = req.body;
+
+    const userExists = await User.findOne({ email });
+
+    if (userExists) {
+        res.status(400);
+        throw new Error('User already exists');
+    }
+
+    const user = await User.create({
+        name,
+        email,
+        password
+    });
+
+    if (user) {
+        res.status(201).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            isAdmin: user.isAdmin,
+        });
+    } 
 });
 
 // @desc    Logout user / clear cookie
 // @route   POST /api/users/logout
 // @access  Private
 const logoutUser = asyncHandler(async (req, res) => {
-    res.send('logout user');
+    res.cookie('jwt', '', {
+        httpOnly: true,
+        expires: new Date(0)
+    });
+
+    res.status(200).json({ message: 'Logged out successfully' });
 });
 
 // @desc    Get user profile
